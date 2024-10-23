02be487b7b133dbbc5dd2c0badaa3a7ec846beeb
swb-buildarchetype <sam@wardbiddle.xyz>
2024-10-23 12:04:39 -0400

    Comment out cart router issue.

--- src/server.ts
+++ src/server.ts
@@ -60,7 +60,7 @@ const start = async () => {
 
   const cartRouter = express.Router();
 
-  cartRouter.use(payload.authenticate);
+  // cartRouter.use(payload.authenticate);
 
   cartRouter.get("/", (req, res) => {
     const request = req as PayloadRequest;
